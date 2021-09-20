28a8cd3b0e4996542572d3f24708156611681b1b
Alex Prykhodko <alex.prykh96@gmail.com>
2021-09-20 15:35:42 +0300

    learning

--- src/library/UserInterface.java
+++ src/library/UserInterface.java
@@ -3,6 +3,7 @@ package library;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -31,15 +32,24 @@ public class UserInterface {
                 break;
             }
 
-            if(input.isEmpty()) {
+            if (input.isEmpty()) {
                 continue;
             }
 
-            if ("add".equalsIgnoreCase(input)){
-                // method
+            if ("add".equalsIgnoreCase(input)) {
+                add();
             }
 
             if ("remove".equalsIgnoreCase(input)) {
+                System.out.println("Which deletion option do you want to use? +" +
+                        "\n- isbn - this option is to deleting the book by isbn;" +
+                        "\n- index - this option is to deleting the book by index.");
+                String isbn = scanner.nextLine();
+                if (isbn.isEmpty()) {
+                    System.err.println("Error! Incorrectly entered data!");
+                }
+                if ("isbn".equalsIgnoreCase(isbn)) {
+                }
                 // method
             }
 
@@ -54,6 +64,40 @@ public class UserInterface {
                 "\n- add - using this command you can add a book to the library;" +
                 "\n- remove - using this command you can delete a book the library;" +
                 "\n- search - using this command you can search a book the library.");
+    }
+
+    private void add() {
+        while (true) {
+            System.out.println("Please, enter a name book: ");
+            String nameBook = scanner.nextLine();
+            if (nameBook.isEmpty()) {
+                System.err.println("Book title cannot be empty!");
+                continue;
+            }
+
+            System.out.println("Please, enter a year of publication:");
+            int yearOfPublication = Integer.parseInt(scanner.nextLine());
+            if (yearOfPublication <= 0) {
+                System.err.println("The year of publication cannot be negative!");
+                continue;
+            }
+
+            System.out.println("Please, enter a author name:");
+            String authorName = scanner.nextLine();
+            if (authorName.isEmpty()) {
+                System.err.println("Author name cannot be empty!");
+                continue;
+            }
+
+            System.out.println("Please, enter a ISBN (identifier serial book number):");
+            int isbn = Integer.parseInt(scanner.nextLine());
+            if (isbn <= 0) {
+                System.err.println("identifier serial book number cannot be 0 or negative!");
+                continue;
+            }
+
+            books.add(new Book(nameBook, yearOfPublication, authorName, isbn));
+        }
     }
 }
 
